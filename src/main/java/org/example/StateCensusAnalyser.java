b5d1378bda1595aa2fd694f15eb341a84abe4b99
Prashanth-Shidigunde <shidigundeprashanth@gmail.com>
2026-06-27 12:17:57 +0530

    "TestCase 1.3 - check with incorrect if extension mismatch"

--- src/main/java/org/example/StateCensusAnalyser.java
+++ src/main/java/org/example/StateCensusAnalyser.java
@@ -9,57 +9,46 @@ import java.util.Iterator;
 
 public class StateCensusAnalyser {
 
-    // Method to load CSV data
-    public int loadStateCensusData(String csvFilePath) throws CensusAnalyserException {
+    public int loadStateCensusData(String csvFilePath)
+            throws CensusAnalyserException {
 
-        // Open the CSV file for reading
         try {
+
+            // Check whether the file is a CSV
+            if (!csvFilePath.endsWith(".csv")) {
+                throw new CensusAnalyserException(
+                        "Invalid File Type",
+                        CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE);
+            }
+
             Reader reader = new FileReader(csvFilePath);
 
-            /*
-             * CsvToBeanBuilder converts every row
-             * of the CSV into a CSVStateCensus object.
-             */
             CsvToBean<CSVStateCensus> csvToBean =
                     new CsvToBeanBuilder<CSVStateCensus>(reader)
                             .withType(CSVStateCensus.class)
                             .withIgnoreLeadingWhiteSpace(true)
                             .build();
 
-            // Create an Iterator to read records one by one
             Iterator<CSVStateCensus> iterator = csvToBean.iterator();
 
             int count = 0;
 
-            // Read each record until no more data is available
             while (iterator.hasNext()) {
-
-                // Get one record
-                CSVStateCensus census = iterator.next();
-
-                /*
-                 * Optional:
-                 * Print every record to verify the data
-                 */
-                System.out.println(
-                        census.state + " | "
-                                + census.population + " | "
-                                + census.areaInSqKm + " | "
-                                + census.densityPerSqKm);
-
-                // Increase record count
+                iterator.next();
                 count++;
             }
-            // Close the file
+
             reader.close();
 
-            // Return total records loaded
             return count;
-        }
-        catch(Exception e){
-                throw new CensusAnalyserException("Incorrect CSV Files");
-        }
 
+        } catch (CensusAnalyserException e) {
+            throw e;
 
+        } catch (Exception e) {
+            throw new CensusAnalyserException(
+                    "State Census File Problem",
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        }
     }
 }
