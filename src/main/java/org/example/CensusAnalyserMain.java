8ee1185c8f61726386adfa08da2c6a5dfe7444f4
Prashanth-Shidigunde <shidigundeprashanth@gmail.com>
2026-06-27 11:42:56 +0530

    "Welcome to Indian States Census Analyser"

---
+++ src/main/java/org/example/CensusAnalyserMain.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class CensusAnalyserMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+    }
+}
